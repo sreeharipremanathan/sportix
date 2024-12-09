a89da3f8c18392faf6bf4ec565cbcae695e7d72b
sreeharipremanathan <sreeharipremanathan@gmail.com>
2024-12-09 23:41:03 +0530

    sportix update

--- sportix_project/app/models.py
+++ sportix_project/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+# class Product(models.Model):
+#     pro_name=models.CharField()
+#     image=models.FileField()
+#     price=models.IntegerField()
+#     offer_price=models.IntegerField()
+
+
+# class Cart(models.Model):
+#     user=models.ForeignKey(User,on_delete=models.CASCADE)
+#     product=models.ForeignKey(products,on_delete=models.CASCADE)
+
+# class buy(models.Model):
